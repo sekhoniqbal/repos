4739403e12bb24a7799612c5401fabb71a50e838
sekho <sekho@LAPTOP-E0GN9UKP>
2018-08-06 01:51:17 -0600

    update 3

--- Test/Models/User.cs
+++ Test/Models/User.cs
@@ -14,8 +14,9 @@ namespace Test.Models
     
     public partial class User
     {
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public int id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string groupname { get; set; }
     }
 }
